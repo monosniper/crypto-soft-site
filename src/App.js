86b7387637cdb3bbc93e2d3eb50acc047620bee5
monosniper <vanteateam@gmail.com>
2023-10-13 10:13:47 +0500

    Revert "sdf"
    This reverts commit 3860566b8c51acce62e90245e925c7788c92d539.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Miner from "./components/Miner";
 import "./styles/App.scss"
+import 'react-tooltip/dist/react-tooltip.css'
+import {observer} from "mobx-react-lite";
+import Login from "./Login";
+import store from "./store";
+import {useEffect} from "react";
 
 function App() {
-  return <div className="App">
-      <Header />
+    useEffect(() => {
+        console.log(store.user)
+    }, [])
 
+  return store.user ? <div className="App">
+      <Header />
+      <Miner />
       <Footer />
-  </div>;
+  </div> : <Login />;
 }
 
-export default App;
+export default observer(App);
